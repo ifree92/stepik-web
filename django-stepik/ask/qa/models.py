7b654396c7d7e5175e4c224306ecb13cddf0ff8c
ifree <ifree92@gmail.com>
2017-01-26 01:12:37 +0200

    fix: QuestionManager

--- django-stepik/ask/qa/models.py
+++ django-stepik/ask/qa/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class QuestionManager(models.Manager):
+
+    def new(self):
+        return Question.objects.order_by('-added_at')
+
+    def popular(self):
+        return Question.objects.order_by('-rating')
+
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
@@ -9,6 +17,7 @@ class Question(models.Model):
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name="q_to_likes")
+    objects = QuestionManager()
 
 
 class Answer(models.Model):
@@ -17,12 +26,3 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, null=False, on_delete=models.CASCADE)
     author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
 
-
-class QuestionManager(models.Manager):
-
-    def new():
-        return Question.objects.order_by('-added_at')
-
-    def popular(self):
-        return Question.objects.order_by('-rating')
-        pass
