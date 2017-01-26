95db4c1e685d6ea461e4555e66f0fd76c2932791
ifree <ifree92@gmail.com>
2017-01-26 13:41:37 +0200

    feat: remove mysql password for stepik container

--- django-stepik/init.sh
+++ django-stepik/init.sh
@@ -6,3 +6,4 @@ sudo cp nginx.conf /etc/nginx/sites-enabled/nginx.conf
 sudo /etc/init.d/nginx restart
 mkdir ~/web
 cp -r ask ~/web/
+sed -i "s/'PASSWORD': '123'/'PASSWORD': ''/g" ~/web/ask/ask/settings.py
