ad2eb37f19107732da1c5c52f11d166c5441bc23
ifree92@gmail.com <46GEBvB9o>
2017-02-03 15:44:31 +0200

    feat: added pymysql instead mysqldb requires to work with mysql database;

--- django-stepik/ask/ask/settings.py
+++ django-stepik/ask/ask/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test_db',
         'USER': 'root',
-        'PASSWORD': '123',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
